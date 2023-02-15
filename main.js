6d4f172f62e5a16706b6fee38e4d0549f6b83ba6
SaifeddineCode <106547346+SaifeddineCode@users.noreply.github.com>
2023-02-15 12:30:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// let darkMode = document.querySelector(".logo-theme .dark-mode")
+// let body = document.querySelector("body")
+// let i = document.querySelectorAll("i")
+
+// darkMode.addEventListener("click",function(){
+//     body.classList.toggle("active")
+//     i.forEach(e=>{
+//         e.classList.toggle("active")
+//     })
+// })
